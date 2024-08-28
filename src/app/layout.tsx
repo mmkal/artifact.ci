cb2edb3339b998c5f37dcd76e4c8c689686fe709
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-08-28 08:58:38 -0400

    lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,22 @@
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
+import {productionUrl} from '../site-config'
 
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'github.mmkal.com',
-  description: 'GitHub Tools by @mmkal',
+  title: productionUrl.hostname,
+  description: 'Artifact Browser by @mmkal',
 }
 
 export default function RootLayout({children}: {children: React.ReactNode}): JSX.Element {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- src/site-config.ts
+++ src/site-config.ts
@@ -1,3 +1,3 @@
-export const productionUrl = new URL('https://github.mmkal.com')
+export const productionUrl = new URL('https://artifact.ci')
 export const twitterUrl = new URL('https://x.com/mmkalmmkal')
 export const githubUrl = new URL('https://github.com/mmkal/artifact-browser')
