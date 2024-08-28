d5de3f16f1bb7b8d4faaafea9462e474e7d034de
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-08-28 14:27:04 -0400

    log

--- upload/upload-file.ts
+++ upload/upload-file.ts
@@ -2,6 +2,29 @@ import {upload} from '@vercel/blob/client'
 import {readFile} from 'fs/promises'
 
 export const uploadFile = async (params: {filepath: string; prefix: string}) => {
+  const url = `${params.prefix}/${params.filepath}`
+  console.log({url, params})
+  //   const EventTypes = {
+  //     generateClientToken: 'blob.generate-client-token',
+  //     uploadCompleted: 'blob.upload-completed',
+  //   }
+  //   const event = {
+  //     type: EventTypes.generateClientToken,
+  //     payload: {
+  //       pathname: params.filepath,
+  //       callbackUrl: url,
+  //       clientPayload: options.clientPayload,
+  //       multipart: options.multipart,
+  //     },
+  //   }
+  //   const res = await fetch(url, {
+  //     method: 'POST',
+  //     body: JSON.stringify(event),
+  //     headers: {
+  //       'content-type': 'application/json',
+  //     },
+  //     signal: options.abortSignal,
+  //   })
   const result = await upload(`${params.prefix}/${params.filepath}`, await readFile(params.filepath), {
     access: 'public',
     handleUploadUrl: '/api/artifact/upload/signed-url',
