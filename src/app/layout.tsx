b3d82edc1d350762e321ce68e341d84abe49b7df
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-08-28 08:40:11 -0400

    pngs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}): JSX.Element {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
