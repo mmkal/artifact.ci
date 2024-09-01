17d0cefc17b093349087744515f1dee249d436d7
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-09-01 11:01:47 -0700

    working-directory (updated 12:29)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({children}: {children: React.ReactNode}): JSX
       <head>
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗿</text></svg>"
         />
       </head>
       <body className={inter.className}>{children}</body>
--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,14 @@ const config: DocsThemeConfig = {
     icon: <img width={20} height={20} src="/x-logo/logo.svg" alt="mmkal on X" />,
   },
   docsRepositoryBase: githubUrl.href + '/tree/main/src/pages',
+  head: () => (
+    // https://www.jacobparis.com/content/use-an-emoji-favicon
+    <link
+      rel="icon"
+      type="image/svg+xml"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗿</text></svg>"
+    />
+  ),
   footer: {
     text: productionUrl.hostname,
   },
