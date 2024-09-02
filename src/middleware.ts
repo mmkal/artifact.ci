6d24f36d68e25f3b642394ff5ee33cbdd08cd4a4
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-09-02 05:58:42 -0700

    eslint-config-inspector --base + index.html probe

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,29 +13,35 @@ export async function middleware(request: NextRequest) {
 
     const pathname = request.nextUrl.pathname.slice(artifaceBlobPrefix.length)
     const targetUrl = new URL(pathname, storageOrigin)
-    const filename = pathname.split('/').pop() || 'file'
-    const mimeType = mime.getType(filename) || 'application/octet-stream'
 
     // todo: get off vercel storage and use our own storage. vercel storage doesn't support inline content: https://vercel.com/docs/storage/vercel-blob#security
     // so we're using a middleware and streaming the response to the client. this will get expensive.
-    const storageResponse = await fetch(targetUrl)
+    let storageResponse = await fetch(targetUrl)
+
+    if (storageResponse.status === 404) {
+      // if 404, try serving `/index.html`
+      storageResponse = await fetch(targetUrl.toString().replace(/\/?$/, '/index.html'))
+    }
 
     if (!storageResponse.ok) {
       return NextResponse.json({error: 'Failed to fetch blob'}, {status: storageResponse.status})
     }
 
-    const isInline =
-      mimeType.startsWith('text/') ||
-      mimeType.startsWith('application/html') ||
-      mimeType.startsWith('application/javascript') ||
-      mimeType.startsWith('application/css') ||
-      mimeType.startsWith('image/') ||
-      mimeType.startsWith('application/pdf')
+    // make sure to get the mime type from the final url, since we might have appended `/index.html`
+    const mimeType = mime.getType(new URL(storageResponse.url).pathname) || 'application/octet-stream'
+
+    // const isInline =
+    //   mimeType.startsWith('text/') ||
+    //   mimeType.startsWith('application/html') ||
+    //   mimeType.startsWith('application/javascript') ||
+    //   mimeType.startsWith('application/css') ||
+    //   mimeType.startsWith('image/') ||
+    //   mimeType.startsWith('application/pdf')
 
     const headers = new Headers(storageResponse.headers)
     headers.set('Content-Type', mimeType)
-    headers.delete('Content-Security-Policy')
-    headers.delete('Content-Disposition')
+    headers.delete('Content-Disposition') // rely on default browser behavior
+    headers.delete('Content-Security-Policy') // be careful!
     // headers.set(
     //   'Content-Security-Policy',
     //   "default-src 'none'; script-src 'none'; style-src 'none'; img-src 'none'; font-src 'none'; connect-src 'none'; frame-src 'none';",
