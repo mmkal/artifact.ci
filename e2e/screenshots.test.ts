96b947ca224b20f1cddea187b38b2ed41ba062da
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-09-03 10:56:23 -0400

    rm abstraction (updated 10:58)

--- e2e/screenshots.test.ts
+++ e2e/screenshots.test.ts
@@ -1,4 +1,4 @@
-import {Page, PageScreenshotOptions, test} from '@playwright/test'
+import {test} from '@playwright/test'
 import {writeFile} from 'node:fs/promises'
 import {setTimeout} from 'node:timers/promises'
 
@@ -13,88 +13,71 @@ test.beforeEach(async ({context}) => {
   ])
 })
 
-const handlers: Record<
-  string,
-  {
-    entrypoint: string
-    handler: (page: Page) => Promise<void>
-    screenshotOptions?: PageScreenshotOptions
-  }
-> = {
-  vitest: {
-    entrypoint: 'vitest/html/index.html',
-    handler: async page => {
-      await page.hover('text=add badly')
-      await page.getByLabel('add badly').getByTestId('btn-open-details').click()
-      await page.click('text=Report')
-      await page.waitForSelector('text=AssertionError')
-    },
-  },
-  playwright: {
-    entrypoint: 'playwright-report/index.html',
-    handler: async page => {
-      await page.click('text=add badly')
-      await page.waitForSelector('text=Error: expect(received).toEqual(expected)')
-    },
-  },
-  jest: {
-    entrypoint: 'report/jest_html_reporters.html',
-    handler: async page => {
-      await page.locator('[data-row-key*="adding.test.js"] [aria-label="Expand row"]').click()
-      await page.waitForSelector('.ant-table-cell:has-text("Passed")')
-      await page.waitForSelector('.ant-table-cell:has-text("Failed")')
-    },
-    screenshotOptions: {fullPage: true},
-  },
-  mocha: {
-    entrypoint: 'output.html',
-    handler: async page => {
-      await page.waitForSelector('text=AssertionError [ERR_ASSERTION]: Expected values to be strictly equal')
-    },
-  },
-  pytest: {
-    entrypoint: 'report/index.html',
-    handler: async page => {
-      await page.waitForSelector('text=test.py:14: AssertionError')
-    },
-  },
-  go: {
-    entrypoint: 'test_report.html',
-    handler: async page => {
-      await page.click('.testResultGroup.failed')
-      await page.click('.testGroupRow.failed')
-      await page.waitForSelector('text=Result was incorrect')
-    },
-  },
-  eslint: {
-    entrypoint: '.eslint-config-inspector',
-    handler: async page => {
-      await page.click('text=eslint/defaults/ignores')
-      await page.waitForSelector('text=Ignore files globally')
-    },
-  },
-  website: {
-    entrypoint: 'demosite/dist/index.html',
-    handler: async page => {
-      await page.waitForSelector('text=Welcome to Starlight')
-    },
-  },
-  pdf: {
-    entrypoint: 'output.pdf',
-    handler: async page => {
-      await page.locator('embed').waitFor()
-      await setTimeout(1000)
-    },
-    screenshotOptions: {
-      clip: {x: 0, y: 0, width: 650, height: 350},
-    },
-  },
-}
+test('take vitest screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/vitest/vitest/html/index.html`)
+  await page.hover('text=add badly')
+  await page.getByLabel('add badly').getByTestId('btn-open-details').click()
+  await page.click('text=Report')
+  await page.waitForSelector('text=AssertionError')
+  await writeFile('public/reports/vitest.png', await page.screenshot())
+})
 
-Object.entries(handlers).forEach(([name, {entrypoint, handler, screenshotOptions}]) => {
-  test(`take ${name} screenshot`, async ({page}) => {
-    await page.goto(`${process.env.BASE_URL}/${name}/${entrypoint}`)
-    await handler(page)
-    await writeFile(`public/reports/${name}.png`, await page.screenshot(screenshotOptions))
-  })
+test('take playwright screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/playwright/playwright-report/index.html`)
+  await page.click('text=add badly')
+  await page.waitForSelector('text=Error: expect(received).toEqual(expected)')
+  await writeFile('public/reports/playwright.png', await page.screenshot())
+})
+
+test('take jest screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/jest/report/jest_html_reporters.html`)
+  await page.locator('[data-row-key*="adding.test.js"] [aria-label="Expand row"]').click()
+  await page.waitForSelector('.ant-table-cell:has-text("Passed")')
+  await page.waitForSelector('.ant-table-cell:has-text("Failed")')
+  await writeFile('public/reports/jest.png', await page.screenshot({fullPage: true}))
+})
+
+test('take mocha screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/mocha/output.html`)
+  await page.waitForSelector('text=AssertionError [ERR_ASSERTION]: Expected values to be strictly equal')
+  await writeFile('public/reports/mocha.png', await page.screenshot())
+})
+
+test('take pytest screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/pytest/report/index.html`)
+  await page.waitForSelector('text=test.py:14: AssertionError')
+  await writeFile('public/reports/pytest.png', await page.screenshot())
+})
+
+test('take go screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/go/test_report.html`)
+  await page.click('.testResultGroup.failed')
+  await page.click('.testGroupRow.failed')
+  await page.waitForSelector('text=Result was incorrect')
+  await writeFile('public/reports/go.png', await page.screenshot())
+})
+
+test('take eslint screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/eslint/.eslint-config-inspector`)
+  await page.click('text=eslint/defaults/ignores')
+  await page.waitForSelector('text=Ignore files globally')
+  await writeFile('public/reports/eslint.png', await page.screenshot())
+})
+
+test('take website screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/website/demosite/dist/index.html`)
+  await page.waitForSelector('text=Welcome to Starlight')
+  await writeFile('public/reports/website.png', await page.screenshot())
+})
+
+test('take pdf screenshot', async ({page}) => {
+  await page.goto(`${process.env.BASE_URL}/pdf/output.pdf`)
+  await page.locator('embed').waitFor()
+  await setTimeout(1000) // I don't think you can really test pdfs with playwright
+  await writeFile(
+    'public/reports/pdf.png',
+    await page.screenshot({
+      clip: {x: 0, y: 0, width: 650, height: 350},
+    }),
+  )
 })
