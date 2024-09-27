7b9602a73a1028839da512af3ea55bee0c3d3bc8
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-09-27 17:41:13 -0400

    wip 17:41

--- src/app/github/events/route.ts
+++ src/app/github/events/route.ts
@@ -4,5 +4,10 @@ export async function POST(request: NextRequest) {
   const body = (await request.json()) as unknown
   console.log('headers', request.headers)
   console.log('event received', request.url, body)
-  return NextResponse.json({})
+  return NextResponse.json({a: 1})
+}
+
+export async function GET(request: NextRequest) {
+  console.log('headers', request.headers)
+  return NextResponse.json({a: 2})
 }
