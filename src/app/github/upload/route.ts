b8771b8fa5bbb14e01adabf96cae9026d82130f9
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-09-28 10:09:50 -0400

    futzing

--- src/app/github/upload/route.ts
+++ src/app/github/upload/route.ts
@@ -1,7 +1,11 @@
 import {NextRequest, NextResponse} from 'next/server'
+import {App} from 'octokit'
+import {z} from 'zod'
 import {fromError} from 'zod-validation-error'
 import {getLogger} from '~/action'
 import {BulkRequest} from '~/types'
+
+export const UploadRequest = z.object({})
 
 export async function POST(request: NextRequest) {
   const logger = getLogger({debug: request.headers.get('artifactci-debug') === 'true'})
@@ -12,12 +16,34 @@ export async function POST(request: NextRequest) {
     logger.error({readable, body: rawBody})
     return NextResponse.json({error: readable.message}, {status: 400})
   }
-
   const body = parsed.data
+  const {context} = body.clientPayload
+  const [owner, repo] = context.repository.split('/')
   logger.debug({body})
+
+  const env = Env.parse(process.env)
+  const app = new App({
+    appId: env.GITHUB_APP_ID,
+    privateKey: env.GITHUB_APP_PRIVATE_KEY,
+  })
+
+  // is there really no way to get an installation by repo name?
+  const {data: allInstallations} = await app.octokit.rest.apps.listInstallations()
+  const installation = allInstallations.find(i => i.account?.login === owner)
+  if (!installation) {
+    return NextResponse.json({error: 'installation not found'}, {status: 404})
+  }
+
+  const octokit = await app.getInstallationOctokit(installation.id)
+  const {data: repos} = await octokit.rest.apps.listReposAccessibleToInstallation()
 
   // todo: get a token for the github app like this does: https://github.com/actions/create-github-app-token
   // docs which ref that: https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/making-authenticated-api-requests-with-a-github-app-in-a-github-actions-workflow
 
   return NextResponse.json({a: 1})
 }
+
+const Env = z.object({
+  GITHUB_APP_ID: z.string(),
+  GITHUB_APP_PRIVATE_KEY: z.string(),
+})
