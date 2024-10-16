ec8c16f33eabb91d19ccc35e6eff2c0e1e85720b
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-10-16 13:43:08 -0400

    lint

--- src/app/artifact/view/[owner]/page.tsx
+++ src/app/artifact/view/[owner]/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {SearchReposUI, SearchUI} from '../Search.server'
+import {SearchReposUI} from '../Search.server'
 import {ArtifactViewPageTemplate} from '../nav'
 import {PathParams} from '../params'
-import {searchArtifacts, searchRepos} from '../search'
+import {searchRepos} from '../search'
 
 export default async function ArtifactViewPage({params}: {params: Partial<PathParams>}) {
   const results = await searchRepos(params)
