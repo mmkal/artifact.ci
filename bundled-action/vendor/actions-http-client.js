5da99a9a8b7a63b6e1345be2004c23ef0910d7c1
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2026-04-28 13:42:37 +0100

    build: collapse action dependencies into shared vendor bundle

--- bundled-action/vendor/actions-http-client.js
+++
@@ -1,2 +0,0 @@
-import {createRequire} from "node:module";const require=createRequire(import.meta.url);
-import{b as m}from"./chunks/chunk-KAKLQET7.js";import{e as o}from"./chunks/chunk-ARRDODK5.js";var t=o(m(),1),p=t.HttpClient;export{p as HttpClient};
